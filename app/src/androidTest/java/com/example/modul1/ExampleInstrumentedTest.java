bdbe5d9c49d50e65494645807e5534b8ae49c9a6
putrihestyh <44332443+putrihestyh@users.noreply.github.com>
2020-04-20 17:23:40 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/modul1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/modul1/ExampleInstrumentedTest.java
@@ -24,4 +24,14 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.modul1", appContext.getPackageName());
     }
+public Classs ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.modul1", appContext.getPackageName());
+    }
+	@Test
+
 }
